478decebbd43f720577f080d579654d434bd6c91
Yassine <awsmdud@gmail.com>
2025-01-31 21:05:09 -0800

    enabled google sign in

--- src/screens/SignIn.jsx
+++ src/screens/SignIn.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
-import { AppleButton, appleAuth } from '@invertase/react-native-apple-authentication';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Platform} from 'react-native';
+import {
+  AppleButton,
+  appleAuth,
+} from '@invertase/react-native-apple-authentication';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
+
+GoogleSignin.configure();
+
+async function onGoogleButtonPress() {
+  // Check if your device supports Google Play
+  await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+  // Get the users ID token
+  const signInResult = await GoogleSignin.signIn();
+
+  // Try the new style of google-sign in result, from v13+ of that module
+  idToken = signInResult.data?.idToken;
+  if (!idToken) {
+    // if you are using older versions of google-signin, try old style result
+    idToken = signInResult.idToken;
+  }
+  if (!idToken) {
+    throw new Error('No ID token found');
+  }
+
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(
+    signInResult.data.idToken,
+  );
+
+  // Sign-in the user with the credential
+  return auth().signInWithCredential(googleCredential);
+}
 
 async function onAppleButtonPress() {
   try {
@@ -14,43 +49,50 @@ async function onAppleButtonPress() {
       throw new Error('Apple Sign-In failed - no identity token returned');
     }
 
-    const { identityToken, nonce } = appleAuthRequestResponse;
-    const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
+    const {identityToken, nonce} = appleAuthRequestResponse;
+    const appleCredential = auth.AppleAuthProvider.credential(
+      identityToken,
+      nonce,
+    );
 
     await auth().signInWithCredential(appleCredential);
-    console.log('Apple sign-in complete!');
   } catch (error) {
     console.error('Apple sign-in error:', error);
   }
 }
 
-const SignInScreen = ({ navigation }) => {
+const SignInScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Arrahman Tour App</Text>
       <Text style={styles.subtitle}>Sign in to continue</Text>
-      {Platform.OS === 'ios' && (<View>
-        <AppleButton
-          buttonStyle={AppleButton.Style.BLACK}
-          buttonType={AppleButton.Type.SIGN_IN}
-          style={styles.appleButton}
-          onPress={onAppleButtonPress}
-        />
-      </View>
-      )}
-      {/* Placeholder for Google Sign In button*/}
-      {/* <TouchableOpacity
-        style={styles.signInButton}
-        onPress={() => console.log('Google sign-in pressed')}
-      >
-        <Text style={styles.signInButtonText}>Sign in with Google</Text>
-      </TouchableOpacity> */}
+      {
+        //if platform is ios, show apple sign in button
+        Platform.OS === 'ios' ? (
+          <View>
+            <AppleButton
+              buttonStyle={AppleButton.Style.BLACK}
+              buttonType={AppleButton.Type.SIGN_IN}
+              style={styles.appleButton}
+              onPress={onAppleButtonPress}
+            />
+          </View>
+        ) : null
+      }
+
+      {/*Google Sign In button*/}
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={() => {
+          onGoogleButtonPress();
+        }}
+      />
 
       {/* Navigate to Email Sign-In Screen */}
       <TouchableOpacity
         style={styles.signInButton}
-        onPress={() => navigation.navigate('Email Sign In')}
-      >
+        onPress={() => navigation.navigate('Email Sign In')}>
         <Text style={styles.signInButtonText}>Sign in with Email</Text>
       </TouchableOpacity>
     </View>
